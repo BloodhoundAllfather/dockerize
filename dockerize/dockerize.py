4a91fab31f0097598c39d82768c405e14186c19b
Vitaliy Geraymovych <vitaliy@geraymovych.com>
2017-12-02 13:08:36 -0800

    add debug info. take 2.

--- dockerize/dockerize.py
+++ dockerize/dockerize.py
@@ -171,8 +171,10 @@ class Dockerize(object):
         LOG.info('copying file %s to %s', src, dst)
         target = os.path.join(self.targetdir, dst[1:])
         target_dir = os.path.dirname(target)
-        source_dir = os.path.dirname(source)
+        source_dir = os.path.dirname(src)
         source_mode = os.stat(source_dir)
+        LOG.debug('target %s', target)
+        LOG.debug('src %s', src)
         LOG.debug('source mode %s', stat.S_IMODE(source_mode))
 
         self.makedirs(target_dir)
